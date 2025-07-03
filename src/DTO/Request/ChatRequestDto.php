9bedb7042d384475397dca3393279e292091a29c
Philippe <28437408+HDLuckySlevin@users.noreply.github.com>
2025-07-03 15:09:55 +0200

    Document ApiSearchController with OpenAPI

--- src/DTO/Request/ChatRequestDto.php
+++ src/DTO/Request/ChatRequestDto.php
@@ -2,8 +2,17 @@
 
 namespace App\DTO\Request;
 
+use OpenApi\Attributes as OA;
+
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[OA\Schema(
+    schema: 'ChatRequestDto',
+    required: ['message'],
+    properties: [
+        new OA\Property(property: 'message', type: 'string', example: 'smartphone')
+    ]
+)]
 readonly class ChatRequestDto
 {
     #[Assert\NotBlank(message: "Message parameter is required")]
