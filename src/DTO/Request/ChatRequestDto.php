beb09e68680fb784c57119897cb6f37e17215a99
Philippe <28437408+HDLuckySlevin@users.noreply.github.com>
2025-07-23 13:27:45 +0200

    feat: add search history support

--- src/DTO/Request/ChatRequestDto.php
+++ src/DTO/Request/ChatRequestDto.php
@@ -10,7 +10,8 @@ use Symfony\Component\Validator\Constraints as Assert;
     schema: 'ChatRequestDto',
     required: ['message'],
     properties: [
-        new OA\Property(property: 'message', type: 'string', example: 'smartphone')
+        new OA\Property(property: 'message', type: 'string', example: 'smartphone'),
+        new OA\Property(property: 'use_history', type: 'boolean', example: false)
     ]
 )]
 readonly class ChatRequestDto
@@ -19,14 +20,18 @@ readonly class ChatRequestDto
     #[Assert\Type("string")]
     public string $message;
 
+    #[Assert\Type("bool")]
+    public bool $use_history;
+
     /****
      * Initializes a new ChatRequestDto with the provided message.
      *
      * @param string $message The chat message content. Defaults to an empty string.
      */
-    public function __construct(string $message = '')
+    public function __construct(string $message = '', bool $use_history = false)
     {
         $this->message = $message;
+        $this->use_history = $use_history;
     }
 
 }
